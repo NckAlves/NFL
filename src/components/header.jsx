d896316c250be2fb1d1f408459820be36018e75b
NckAlves <nicholasoxalves@gmail.com>
2022-02-03 01:38:08 -0300

    exibicao finalizada

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.css'
+
+export default () =>
+
+    <>
+        <div className='header'>
+            <h1 className='Titulo'>Bem-Vindo à NFL!</h1>
+            <div className='Texto'>
+                <p>Conheça as equipes da principal liga dos esportes americanos</p>
+                <p>Bastas clicar no Card para ver as principais informações sobre cada equipe</p>
+
+            </div>
+        </div>
+
+    </>
+
